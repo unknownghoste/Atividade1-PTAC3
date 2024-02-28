2354ee66e3d6a657d1b51912e79cb9792e41c59b
unknownghoste <99215178+unknownghoste@users.noreply.github.com>
2024-02-28 14:39:23 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+    domains: [
+        "www.ifms.edu.br"
+        //colocar dominio do moodle para atividade. não precisa de https. fazer footer com link do moodle.
+    ]
+}
+};
+
+export default nextConfig;
+
